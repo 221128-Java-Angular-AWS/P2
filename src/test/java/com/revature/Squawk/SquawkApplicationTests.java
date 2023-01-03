e551a3f7d93c5634986b3dab86908cd3dc00dbce
Audie Ni <111254997+tasukaru@users.noreply.github.com>
2023-01-03 16:01:30 -0500

    added spring to java project

---
+++ src/test/java/com/revature/Squawk/SquawkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Squawk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquawkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
