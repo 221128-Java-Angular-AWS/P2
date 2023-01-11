c18f5f3294acba34c8e0b77e220412f3139bc7f9
Travis Morse <email@example.com>
2023-01-10 23:22:55 -0600

    Implemented embedded Youtube videos

--- src/ui/src/app/post/post.component.ts
+++ src/ui/src/app/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input  } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 import { Post, PostsService } from '../Services/posts.service';
 import { Comment } from '../comment';
 
@@ -12,8 +13,17 @@ export class PostComponent {
   @Input() removePostFromFeed!: Function;
   comments: Comment[] = [];
 
-  constructor(private postsService: PostsService) {
+  constructor(private postsService: PostsService, public _sanitizer: DomSanitizer) {
 
+  }
+
+  embedYoutube: Boolean = false;
+  youtubeLink!: string;
+  safeYoutubeLink!: SafeResourceUrl;
+
+  ngOnInit(): void {
+    this.youtubeLink = this.parseForYoutube(this.post.message) || this.youtubeLink;
+    this.safeYoutubeLink = this._sanitizer.bypassSecurityTrustResourceUrl(this.youtubeLink);
   }
 
   clickMoreComments(post: Post): void {
@@ -23,5 +33,19 @@ export class PostComponent {
   deletePostById(postId: number): void {
     this.postsService.deletePostById(postId);
     this.removePostFromFeed(postId);
+  }
+
+   parseForYoutube(message: String) {
+    if (message.includes("youtube.com/watch?v=")) {
+      this.embedYoutube = true;
+      let cutoff = message.lastIndexOf("youtube.com/watch?v=") + 20;
+      let valid = /^[a-zA-Z0-9_]*$/;
+      for (let i = cutoff; i < message.length; i++) {
+        if (!message[i].match(valid)) {
+          return "https://youtube.com/embed/" + message.slice(cutoff, i);
+        }
+      } return "https://youtube.com/embed/" + message.slice(cutoff);
+    }
+    return '';
   }
 }
