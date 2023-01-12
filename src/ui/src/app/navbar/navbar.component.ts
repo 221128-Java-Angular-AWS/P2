6b54c020ac1873c5353077f2037af54507c87d8a
Braydensn <braydensn@gmail.com>
2023-01-12 11:18:05 -0600

    Added a small message when the search bar doesn't find any users

--- src/ui/src/app/navbar/navbar.component.ts
+++ src/ui/src/app/navbar/navbar.component.ts
@@ -9,10 +9,12 @@ import { User } from 'app/model/user';
 })
 export class NavbarComponent {
   users: User[] = [];
+  noResults = false;
 
   constructor(private userService: UsersService){}
 
   searchForUsers(filter: string){
+    this.noResults = false;
     if(filter == ""){
       this.users = [];
     }
@@ -20,6 +22,9 @@ export class NavbarComponent {
       this.userService.searchUsers(filter)
       .subscribe((users) => {
         this.users = users;
+        if(users.length == 0){
+          this.noResults = true;
+        }
       });
     }
   }
@@ -27,5 +32,6 @@ export class NavbarComponent {
   resetSearch(){
     this.users = [];
     (<HTMLInputElement>document.getElementById("searchbar")).value = "";
+    this.noResults = false;
   }
 }
