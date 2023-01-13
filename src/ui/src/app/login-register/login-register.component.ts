d21d652f2cbb5c1e2879f0c611b11a42f865f63d
matt-shen <90221619+matt-shen@users.noreply.github.com>
2023-01-13 15:31:50 -0500

    bugs and error handling

--- src/ui/src/app/login-register/login-register.component.ts
+++ src/ui/src/app/login-register/login-register.component.ts
@@ -38,6 +38,8 @@ export class LoginRegisterComponent {
       this.cookieService.setCurrentUser(user);
       this.router.navigate(['/home']);
       console.log("posted user: "+ JSON.stringify(user));
+    }, err =>{
+      alert('this username has already been taken');
     })
     this.registerForm.reset();
   }
